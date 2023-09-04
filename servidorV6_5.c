4b037f7bffcb26cbc9afbdce53ab29ceb1d22cb4
Jaume Vintró <jvintrom@gmail.com>
2023-09-04 09:43:41 +0200

    primer cambio github

--- servidorV6_5.c
+++ servidorV6_5.c
@@ -8,6 +8,7 @@
 #include <mysql.h>
 //#include <mysql.h>
 //#include <mysql.h>
+//#include <mysql.h>
 #include <pthread.h>
 //Estructura necesaria para acceso excluyente
 pthread_mutex_t mutex =PTHREAD_MUTEX_INITIALIZER;
